ffaa3cd4f63cf36afe78d1efa6e8c28168e3749b
saran-github <saranb26@gmail.com>
2015-05-19 01:01:34 +0530

    changes

--- application/views/admin/featured_products/index.php
+++ application/views/admin/featured_products/index.php
@@ -2,6 +2,7 @@
 <link rel="stylesheet" href="<?php echo base_url();?>plugins/datatables/media/css/jquery.dataTables.css" type="text/css" />
 <link rel="stylesheet" href="<?php echo base_url();?>plugins/datatables/media/css/jquery.dataTables_themeroller.css" type="text/css" />
 <script src="<?php echo base_url();?>plugins/datatables/media/js/jquery.dataTables.js" type="text/javascript"></script>
+<script src="<?php echo base_url();?>js/admin/featured_product.js" type="text/javascript"></script>
 <nav>
 <?php $this->load->view('admin/left_menu'); ?> 
 </nav>
